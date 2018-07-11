ed953bb830dd00641a6f47ec3cd4b43d8bd92523
Andrew Och <andrewoch95@gmail.com>
2018-07-11 15:18:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,7 +14,9 @@ end
 def get_contestant_name(data, occupation)
   data.each do |season, contestants_array|
     contestants_array.each do |contestant|
-      if contestant{:
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
     end
   end
 end
